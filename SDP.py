b774bb4b2ae2c0de8661ccac93df22f607df92b1
Siddhartha Khanooja <siddhartha.khanooja@gmail.com>
2017-04-04 00:34:19 +0530

    Added support for Python 2.7

--- SDP.py
+++ SDP.py
@@ -3,6 +3,7 @@ def SDP(str):
 	dep_parser=StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
 	xyz = [parse.tree() for parse in dep_parser.raw_parse(str)]
 	print(xyz)
+       #print xyz (for Python 2.7)
 
 if __name__ == "__main__":
     import sys
